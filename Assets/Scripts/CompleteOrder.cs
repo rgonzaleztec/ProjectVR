29b0f942accedca38b7ab0a3f93ed16297004f21
Reyner-Arias <Rey86581@gmail.com>
2023-01-19 15:24:27 -0600

    Setting third scene to testing

--- Assets/Scripts/CompleteOrder.cs
+++ Assets/Scripts/CompleteOrder.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class CompleteOrder : MonoBehaviour
 {
-    public Animator dummyAnimator;
+
+    [SerializeField] GameObject billPrefab;
+    private Animator dummyAnimator;
+    private System.String parentName;
+    private GameObject dummy;
+    private GameObject bill;
+
+    void Start(){
+        parentName = this.transform.parent.name;
+        dummy = GameObject.Find(parentName+"DummyPrefab(Clone)");
+        dummyAnimator = dummy.GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.tag == "Cake")
+        if (other.gameObject.tag == "Cake" & dummy != null)
         {
             other.gameObject.GetComponent<Rigidbody>().useGravity = true;
-            other.gameObject.GetComponent<Rigidbody>().isKinematic = false;
+            other.gameObject.GetComponent<Rigidbody>().isKinematic = true;
             other.transform.parent = this.transform.parent;
-            other.gameObject.transform.localPosition = new Vector3(0f, 1.9f, 0f);
+            other.gameObject.transform.localPosition = new Vector3(0f, 1.62f, 0f);
 
             dummyAnimator.SetTrigger("Completed");
+            StartCoroutine(DummyPayment());
         }
+    }
+
+    IEnumerator DummyPayment()
+    {
+        yield return new WaitForSeconds(3.833f);
+
+        bill = Instantiate(billPrefab, dummy.transform.parent.position + new Vector3(0f, 1.62f, 0f), Quaternion.identity);
+        bill.transform.parent = dummy.transform.parent;
+
+        bill.transform.localScale = new Vector3(16f, 16f, 16f);
+
+        Destroy(dummy);
     }
 }
--- Assets/Scripts/PositionDummy.cs
+++ Assets/Scripts/PositionDummy.cs
@@ -7,13 +7,8 @@ public class PositionDummy : MonoBehaviour
 {
 
     [SerializeField] GameObject dummyPrefab;
-    [SerializeField] GameObject billPrefab;
     private GameObject[] sittingPositions;
     private GameObject dummy;
-    private Animator dummyAnimator;
-    //private bool startMoving = false;
-    private CharacterController dummyCC;
-    private GameObject bill;
 
     // Start is called before the first frame update
     void Start()
@@ -27,42 +22,5 @@ public class PositionDummy : MonoBehaviour
 
         dummy.transform.position = sittingPositions[num].transform.position;
         dummy.transform.rotation = sittingPositions[num].transform.rotation;
-
-        dummyCC = dummy.GetComponent<CharacterController>();
-
-        //Thumbs up
-        dummyAnimator = dummy.GetComponent<Animator>();
-        StartCoroutine(CompleteOrder());
     }
-
-    IEnumerator CompleteOrder()
-    {
-        yield return new WaitForSeconds(7f);
-        dummyAnimator.SetTrigger("Completed");
-        StartCoroutine(DummyPayment());
-    }
-
-    IEnumerator DummyPayment()
-    {
-        bill = Instantiate(billPrefab, dummy.transform.parent.position + new Vector3(0f, 1.6f, 0f), Quaternion.identity);
-        bill.transform.parent = dummy.transform.parent;
-
-        bill.transform.localScale = new Vector3(20f, 20f, 20f);
-
-        yield return new WaitForSeconds(3.833f);
-        Destroy(dummy);
-    }
-
-    /*void Update()
-    {
-        if(startMoving){
-            Move();
-        }
-    }
-
-    void Move()
-    {
-        Vector3 _forward = dummy.transform.TransformDirection(Vector3.forward);
-        dummyCC.SimpleMove(_forward * 3);
-    }*/
 }
