e2a52f94db7af459e0aaa9ba60339478e7a9a6dc
Reyner-Arias <Rey86581@gmail.com>
2023-01-19 18:18:34 -0600

    Add music and storyteller voice third scene

--- Assets/Scripts/SaveMoney.cs
+++ Assets/Scripts/SaveMoney.cs
@@ -7,12 +7,14 @@ using TMPro;
 public class SaveMoney : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI text;
+    [SerializeField] AudioSource cashierSound;
     private int coinsQuantity;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Coin")
         {
+            cashierSound.Play();
             Destroy(other);
             coinsQuantity++;
             text.text = "Coins Quantity: " + coinsQuantity;
